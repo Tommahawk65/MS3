aeba8c9f00619ac540d6e3e30100078fa896c1f3
Tommahawk65 <tom.riggs@live.co.uk>
2022-02-16 22:25:29 +0000

    Logout woorking correctly

--- app.py
+++ app.py
@@ -91,13 +91,18 @@ def profile(username):
 #    grab the session user's username from db
     username = mongo.db.users.find_one(
         {"name": session["user"]})["name"]
-    return render_template("profile.html", username=username)
+    
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    
+    return redirect(url_for("login")) 
 
 
 @app.route('/logout')
 def logout():
+    flash("Logged Out")
     session.pop("user")
-    return redirect('/')
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
